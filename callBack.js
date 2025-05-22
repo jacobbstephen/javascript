fb470544b820fb704c3700d275d39598b1b5b02f
JACOB B STEPHEN <jacobstebyron@gmail.com>
2025-05-22 19:25:12 +0530

    Promise .then chainning

---
+++ callBack.js
@@ -0,0 +1,6 @@
+// Just an assignment - Dont use promise
+// 1. Write a function to download data from url
+// 2. Write a function to save the downloaded data onto a file and return the filename
+// 3.  Write a function to upload the file written in previous step to a new url
+
+
